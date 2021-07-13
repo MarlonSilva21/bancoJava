0ef8a5a52045ab161624a171026904bf6a8771c9
joaoffgazzara <joaoffgazzara@gmail.com>
2021-07-12 21:27:27 -0300

    Função Saque Override

--- src/entities/ContaEmpresarial.java
+++ src/entities/ContaEmpresarial.java
@@ -1,8 +1,12 @@
 package entities;
 
+import java.util.Scanner;
+
 public class ContaEmpresarial extends Conta {
 	
+	private double valorLimiteSaldoInsuficiente;
 	private double limiteEmpresarial;
+	Scanner leia = new Scanner(System.in);
 
 	public ContaEmpresarial(int numeroConta, double limiteEmpresarial) {
 		super(numeroConta);
@@ -17,5 +21,36 @@ public class ContaEmpresarial extends Conta {
 		this.limiteEmpresarial = limiteEmpresarial;
 	}
 	
-	
+	@Override
+	public void saque (double valorSaque) {
+		if(valorSaque <= 0) {
+			System.out.println("Operação inválida.");
+		}
+		
+		else if (valorSaque>saldo  && limiteEmpresarial < (valorSaque - saldo)) {
+			System.out.println("Saldo insuficiente.");
+		}
+		
+		else if (valorSaque>saldo && limiteEmpresarial > (valorSaque - saldo)) {
+			System.out.println("Saldo insuficiente.\nGostaria de usar o Limite Especial para completar a transação? [S/N]: ");
+			char usarLimite = leia.next().toUpperCase().charAt(0);
+				if (usarLimite == 'S') {
+				valorLimiteSaldoInsuficiente = saldo - valorSaque;
+				limiteEmpresarial -= valorLimiteSaldoInsuficiente;
+				
+				}
+				else if (usarLimite == 'N') {
+					System.out.println("Transação cancelada.");
+				}
+				
+				else {
+					System.out.println("Opção inválida, cancelando transação.");
+				}
+				
+		}
+		else {
+		this.saldo -= valorSaque;
+		System.out.println("Operação realizada com sucesso.");
+		}
+	}
 }
